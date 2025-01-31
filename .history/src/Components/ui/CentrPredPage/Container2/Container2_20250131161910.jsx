e8062c3bd23b3aafefb3a07083d154e7b1aa9c90
gonov <adamej10@gmail.com>
2025-01-31 17:08:37 +0300

    12

---
+++ .history/src/Components/ui/CentrPredPage/Container2/Container2_20250131161910.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './Container.module.css';
+// import CenterBlock from '../../Standart/CenterBlock/CenterBlock';
+// import WidthBlock from '../../Standart/WidthBlock/WidthBlock';
+
+function Container({ children, ...props }) {
+    return ( 
+        <>
+            
+        </>
+     );
+}
+
+export default Container;
---
+++ .history/src/Components/ui/CentrPredPage/Container2/Container2_20250131162141.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Container2.module.css';
+import CenterBlock from '../../Standart/CenterBlock/CenterBlock';
+import WidthBlock from '../../Standart/WidthBlock/WidthBlock';
+
+function Container2({ children, ...props }) {
+  return (
+    <>
+      <CenterBlock>
+        <WidthBlock>
+            <div className={classes.container}>
+                <div className={classes.containerTitle}>
+                    <span>Здесь можно получить самую различную информацию и услуги, необходимые действующим, начинающим и будущим предпринимателям по различным направлениям: от воплощения идеи до модернизации:</span>
+                </div>
+                <div className={classes.containerBottom}
+            </div>
+        </WidthBlock>
+      </CenterBlock>
+    </>
+  );
+}
+
+export default Container2;
