d7e50d154ab4b2e274749692419d166fa909f9ed
gonov <adamej10@gmail.com>
2025-01-31 17:08:37 +0300

    12

---
+++ .history/src/Components/ui/CentrPredPage/Container2/Container2_20250131161838.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './Empty.module.css';
+// import CenterBlock from '../../Standart/CenterBlock/CenterBlock';
+// import WidthBlock from '../../Standart/WidthBlock/WidthBlock';
+
+function Empty({ children, ...props }) {
+    return ( 
+        <>
+            
+        </>
+     );
+}
+
+export default Empty;
