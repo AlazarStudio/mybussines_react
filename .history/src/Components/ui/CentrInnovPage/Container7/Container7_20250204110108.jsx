4ecd88a856046260d100e009d3f84c4ae2ede2c3
gonov <adamej10@gmail.com>
2025-02-04 12:15:49 +0300

    inn

---
+++ .history/src/Components/ui/CentrInnovPage/Container7/Container7_20250204110108.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './Container7.module.css';
+import CenterBlock from '../../../Standart/CenterBlock/CenterBlock';
+import WidthBlock from '../../../Standart/WidthBlock/WidthBlock';
+
+function Container7({ children, ...props }) {
+  return (
+    <>
+      <CenterBlock>
+        <WidthBlock>
+          <div className={classes.container}>
+       <div className={classes}
+          </div>
+        </WidthBlock>
+      </CenterBlock>
+    </>
+  );
+}
+
+export default Container7;
---
+++ .history/src/Components/ui/CentrInnovPage/Container7/Container7_20250204110216.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Container7.module.css';
+import CenterBlock from '../../../Standart/CenterBlock/CenterBlock';
+import WidthBlock from '../../../Standart/WidthBlock/WidthBlock';
+
+function Container7({ children, ...props }) {
+  return (
+    <>
+      <CenterBlock>
+        <WidthBlock>
+          <div className={classes.container}>
+            <div className={classes.containerTitle}>
+              <span>
+                Существует 4 категории социального предпринимательства, по
+                которым вы сможете определить социальную направленность вашего
+                бизнеса:
+              </span>
+            </div>
+            <div className={classes.containerBlock}>
+              <div className={classes.c}
+            </div>
+          </div>
+        </WidthBlock>
+      </CenterBlock>
+    </>
+  );
+}
+
+export default Container7;
